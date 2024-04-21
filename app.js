853abd7b56db8a0a07272e3a789ce947ffd20c5b
talhayu <mnhetz999@gmail.com>
2024-04-21 18:25:38 +0500

    udpated

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const express=  require('express');
-const app = express()
-require("dotenv").config()
-const PORT = process.env.PORT || 80
-app.get('/', async (req, res)=>{
-    res.send('Hello from aws deployed service')
-})
-app.listen(PORT, ()=>{console.log('Port is listening', PORT)})
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('A Monk in Cloud'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
