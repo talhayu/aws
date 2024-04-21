d12662240d2e46ca6d9e8aa74fa3e595efdf8ae2
talhayu <mnhetz999@gmail.com>
2024-04-21 18:27:52 +0500

    udpatedagain

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var http = require('http');
+const express=  require('express');
+const app = express()
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+const PORT = 8080
+app.get('/', async (req, res)=>{
+    res.send('Hello from aws deployed service')
+})
+app.listen(PORT, ()=>{console.log('Port is listening', PORT)})
