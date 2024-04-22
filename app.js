70b545d396756971d277544bfae3c39f050afb33
talhayu <mnhetz999@gmail.com>
2024-04-22 10:21:46 +0500

    udpated

--- app.js
+++ app.js
@@ -7,18 +7,8 @@ app.get('/', async (req, res) => {
     res.send('Hello from aws deployed service');
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
 
-// Start the server
-const server = app.listen(PORT, () => {
+ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-// Handle server startup errors
-server.on('error', (err) => {
-    console.error('Server startup error:', err);
-});
