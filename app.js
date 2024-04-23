1ffb20ace99852a2a35d38c497a4d646b55798af
Muhammad Talha Ahmed <mnhetz999@gmail.com>
2024-04-23 19:00:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const app = express();
+const aws = require('aws-sdk');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
 
 const PORT = 8080;
+
+aws.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'ap-south-1'
+});
+
+const s3 = new aws.S3();
+
+app.get('/image', async (req, res) => {
+    const bucketName = 'imagefornodejs';
+    const key = 'Products Images941.jpg';
+
+    const params = { Bucket: bucketName, Key: key };
+    const s3Object = await s3.getObject(params).promise();
+
+    res.set('Content-Type', s3Object.ContentType);
+
+    res.send(s3Object.Body);
+});
 
 app.get('/', async (req, res) => {
     res.send('Hello from AWS deployed service');
