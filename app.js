a7830d3e5582b6424e5ac3605c8f5175e3add9c4
talhayu <mnhetz999@gmail.com>
2024-04-23 16:32:42 +0500

    addedAgain

--- app.js
+++ app.js
@@ -7,13 +7,10 @@ app.get('/', async (req, res) => {
     res.send('Hello from AWS deployed service');
 });
 
-<<<<<<< HEAD
 app.get('/new', async(req, res)=>{
     res.send('New route added')
 })
- app.listen(PORT, () => {
-=======
+
 app.listen(PORT, () => {
->>>>>>> 51a12275f680842c03308da4b7947fa3bafcbd5b
     console.log(`Server is running on port ${PORT}`);
 });
