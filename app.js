4c4034fd5a72f2edeeb9c6ee986a5dfd170e4813
Muhammad Talha Ahmed <mnhetz999@gmail.com>
2024-04-23 16:19:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = 8080;
+const PORT = 22;
 
 app.get('/', async (req, res) => {
     res.send('Hello from AWS deployed service');
