b6476e7fc0979fef43600b5c554df05b6b6f8150
talhayu <mnhetz999@gmail.com>
2024-07-28 19:50:18 +0500

    done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.get('/', async (req, res) => {
+    res.send('Hello from AWS deployed service');
+});
+
+app.get('/track_open', async (req, res) => {
+    try {
+        const email = req.query.email;
+        console.log(`Email clicked: ${email}`);
+        const filePath = path.join(__dirname, 'public', 'pixel.png')
+        res.sendFile(filePath)
+    } catch (error) {
+        console.error('Error updating email read status:', error);
+        res.status(500).json({ message: 'Internal server error.' });
+    }
+});
+
+app.listen(5050, () => {
+    console.log(`Server is running on port`);
+});
